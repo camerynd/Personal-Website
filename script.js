6ca6da7580e50e7d928f244d8c327f23e97a3290
github username <github email address>
2021-07-10 11:57:59 -0600

    added JavaScript and upload image, finished website

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This content added by javascript";
-document.querySelector('body').appendChild(h2);
-
+const newElement = document.createElement('p');
+const text = "I'm a programming student interested in software development. I'm learning HTML, CSS, and JavaScript, however I'm currently most familiar with C++. I live in Longmont, CO and am attending school full-time. My hobbies include visual art and crochet. Connect with me via Github or Email!";
+const newElementLocation = document.getElementsByClassName('flex-right')[0];
+newElementLocation.appendChild(newElement);
+newElement.textContent = text;
